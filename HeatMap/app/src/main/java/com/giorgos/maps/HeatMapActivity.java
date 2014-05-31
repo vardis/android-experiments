5bea706d10042bf692d854d51cec17e1b0205f3b
Giorgos Giannoudovardis <vardis.g@gmail.com>
2014-05-31 18:41:42 +0200

    Adds a sample for rendering heatmaps on top of Google Maps

---
+++ HeatMap/app/src/main/java/com/giorgos/maps/HeatMapActivity.java
@@ -0,0 +1,172 @@
+package com.giorgos.maps;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.giorgos.maps.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class HeatMapActivity extends ActionBarActivity implements GoogleMap.OnMapClickListener {
+
+    public final static String LOG_TAG = HeatMapActivity.class.getName();
+
+    private GoogleMap mMap;
+
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
+
+    private TextView mLocationText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLocationText = (TextView) findViewById(R.id.locationText);
+
+        initialiseMap();
+
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        Log.i(LOG_TAG, "Memory class: " + manager.getMemoryClass());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initialiseMap();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initialiseMap() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.setOnMapClickListener(this);
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+        addHeatMap();
+    }
+
+    private void addHeatMap() {
+        List<LatLng> list = null;
+
+        // Get the data: latitude/longitude positions of police stations.
+        try {
+            list = readItems(R.raw.stations);
+        } catch (JSONException e) {
+            Toast.makeText(this, "Problem reading list of locations.", Toast.LENGTH_LONG).show();
+        }
+
+        // Create a heat map tile provider, passing it the latlngs of the police stations.
+        mProvider = new HeatmapTileProvider.Builder()
+                .data(list)
+                .build();
+
+        // Add a tile overlay to the map, using the heat map tile provider.
+        mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        mLocationText.setText(latLng.toString());
+        Log.i(LOG_TAG, "CLicked at: " + latLng.toString());
+        float zoomLevel = mMap.getCameraPosition().zoom;
+        LatLng center = mMap.getCameraPosition().target;
+        Log.i(LOG_TAG, "Centered at " + center + " @" + zoomLevel);
+    }
+
+    // Datasets from http://data.gov.au
+    private ArrayList<LatLng> readItems(int resource) throws JSONException {
+        ArrayList<LatLng> list = new ArrayList<LatLng>();
+        InputStream inputStream = getResources().openRawResource(resource);
+        String json = new Scanner(inputStream).useDelimiter("\\A").next();
+        JSONArray array = new JSONArray(json);
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject object = array.getJSONObject(i);
+            double lat = object.getDouble("lat");
+            double lng = object.getDouble("lng");
+            list.add(new LatLng(lat, lng));
+        }
+        return list;
+    }
+
+    /**
+     * Helper class - stores data sets and sources.
+     */
+    private class DataSet {
+        private ArrayList<LatLng> mDataset;
+        private String mUrl;
+
+        public DataSet(ArrayList<LatLng> dataSet, String url) {
+            this.mDataset = dataSet;
+            this.mUrl = url;
+        }
+
+        public ArrayList<LatLng> getData() {
+            return mDataset;
+        }
+
+        public String getUrl() {
+            return mUrl;
+        }
+    }
+
+}
